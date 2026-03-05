f97b3880e313e533eee942a6007c343d49b87e35
roscioli04-cmd <tua-email-di-github@esempio.com>
2026-03-05 18:19:27 +0100

    aggiungo i file

---
+++ esercizio_ereditarieta/main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Istanza della classe base
+        Animale mioAnimale = new Animale();
+        
+        // Istanza della sottoclasse
+        Gatto mioGatto = new Gatto();
+
+        // Chiamata dei metodi per verificare l'override
+        System.out.print("Verso generico: ");
+        mioAnimale.faiIlVerso();
+
+        System.out.print("Verso del gatto: ");
+        mioGatto.faiIlVerso();
+    }
+
+    }
