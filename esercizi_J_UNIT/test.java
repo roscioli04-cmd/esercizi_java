ec36e490b688985f480674509b05d3f671717bed
roscioli04-cmd <tua-email-di-github@esempio.com>
2026-04-13 16:56:00 +0200

    aggiunti i file junit

---
+++ esercizi_J_UNIT/test.java
@@ -0,0 +1,15 @@
+package esercizi_J_UNIT;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class test {
+
+
+    public String formattaDataShort(String isoString) {
+        OffsetDateTime data = OffsetDateTime.parse(isoString);
+        return data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.ITALY));
+    }
+}
